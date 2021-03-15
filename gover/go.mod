49ef5936d6bcfcd508d32c2eb2625f9eb5df6dd9
Sachin Sunkle <sachin.sunkle@worldline.com>
2021-03-15 17:05:50 +0530

    Vo adj momemntum + added mage to gover

--- gover/go.mod
+++ gover/go.mod
@@ -4,6 +4,8 @@ go 1.15
 
 require (
 	github.com/antchfx/htmlquery v1.2.3
+	github.com/carolynvs/magex v0.5.0
 	github.com/jackc/pgx/v4 v4.10.1
+	github.com/magefile/mage v1.11.0
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 )
--- pyver/voadjustedmomentum.py
+++ pyver/voadjustedmomentum.py
@@ -3,6 +3,10 @@ from contextlib import closing
 import requests
 import codecs
 import csv
+import numpy as np
+import pandas as pd
+import pandas_datareader.data as web
+import yfinance as yf
 
 # 1. On the first trading day of the month, we rank all stocks based on their
 # “Volatility-Adjusted Momentum”: Absolute return over the last 52 weeks (250 working
@@ -29,19 +33,30 @@ with closing(requests.get(cnxurl, stream=True)) as r:
         todaysprice = p["dayHigh"]
         lowyearly = p["low52"]
         absolutereturns = (todaysprice-lowyearly)/lowyearly
-        symbolList[symbol] = absolutereturns
+        annualV = getannualvolatility(symbol)
+        symbolList[symbol] = absolutereturns/annualV
 
-        # TODO: Calculated Annualized Volatility
-        # STEP 1: Calculate Daily Returns, we know that the daily returns can be calculated as –
-        # Return = LN (Ending Price / Beginning Price), where LN denotes Logarithm to Base ‘e’, note this is also called ‘Log Returns’.
-        # STEP 2: calculate standard deviation for the daily returns --> this will give daily volatility
-        # Step 3: Calculate annual volatility as Daily Volatility * SQRT(252)
-        # STEP 4: Divide Absolutereturns by Annual Volatility
-
-        # counter += 1
-        # if counter > 100:
-        #     break
 
 orderedList = sorted(symbolList.items(), key=lambda kv: kv[1], reverse=True)
-for i in orderedList[:20]:
+for i in orderedList[:30]:
     print(i)
+
+    # TODO: Calculated Annualized Volatility
+    # STEP 1: Calculate Daily Returns, we know that the daily returns can be calculated as –
+    # Return = LN (Ending Price / Beginning Price), where LN denotes Logarithm to Base ‘e’, note this is also called ‘Log Returns’.
+    # STEP 2: calculate standard deviation for the daily returns --> this will give daily volatility
+    # Step 3: Calculate annual volatility as Daily Volatility * SQRT(252)
+    # STEP 4: Divide Absolutereturns by Annual Volatility
+
+
+def getannualvolatility(symbol):
+    tracker = yf.Ticker(symbol+".NS")
+    hist = tracker.history(period="1y")
+
+    # Compute the logarithmic returns using the Closing price
+    hist['Log_Ret'] = np.log(hist['Close'] / hist['Close'].shift(1))
+
+    # Compute Volatility using the pandas rolling standard deviation function
+    annualvolatility = hist.std()["Log_Ret"] * np.sqrt(252)
+
+    return annualvolatility
